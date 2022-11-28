faac45ba981de0efa7c7f50e5daf260e7b289cfc
Kre4 <denhol2002@gmail.com>
2022-11-28 16:41:35 +0300

    fix: delete console.logs

--- lab1-2/scripts/time-load-component.js
+++ lab1-2/scripts/time-load-component.js
@@ -3,12 +3,10 @@
 
     function timer() {
         let element = document.getElementById('load-time');
-        console.log(element)
         element.innerHTML += 'Loaded in ' + (new Date().getTime() - startTime) / 1000 + ' seconds';
     }
 
     window.onload = function () {
         timer();
-        console.log('window on load done')
     }
 })();
