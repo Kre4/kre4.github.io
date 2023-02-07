2b48617ea8861b10f98a140fd76abfbfff2a641d
Kre4 <denhol2002@gmail.com>
2023-02-07 21:38:35 +0300

    feat: abb base nav

--- social-events-manager/frontend/src/app/app.module.ts
+++ social-events-manager/frontend/src/app/app.module.ts
@@ -4,13 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {MatTabsModule} from '@angular/material/tabs';
+import { EventCardComponent } from './event-card/event-card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventCardComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
