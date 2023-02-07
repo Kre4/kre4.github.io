7a1418500d8bafefa545e9dda2a4573eaa1eca43
Kre4 <denhol2002@gmail.com>
2023-02-07 22:43:29 +0300

    feat: add dependencies, add basic structure

--- social-events-manager/frontend/src/app/app.module.ts
+++ social-events-manager/frontend/src/app/app.module.ts
@@ -1,25 +1,37 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 
 import {MatTabsModule} from '@angular/material/tabs';
-import { EventCardComponent } from './event-card/event-card.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {EventCardComponent} from './event-card/event-card.component';
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatDividerModule} from "@angular/material/divider";
+import { EventsListComponent } from './events-list/events-list.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    EventCardComponent
+    EventCardComponent,
+    EventsListComponent
   ],
   imports: [
+    MatCardModule,
     BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
-    MatTabsModule
+    MatTabsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
