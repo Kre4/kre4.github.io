d7bf73ce0008acae010beb5f7ba0871902013845
Kre4 <denhol2002@gmail.com>
2023-10-02 22:11:40 +0300

    feat: add index and base enums

---
+++ sql-to-java/sql-related-enums.js
@@ -0,0 +1,10 @@
+const ResourceEnum = {
+    COLUMN: 'column',
+    CONSTRAINT: 'constraint',
+}
+
+const AstElementType = {
+    COLUMN_REF: 'column_ref',
+    CREATE: 'create',
+    ALTER: 'alter'
+}
